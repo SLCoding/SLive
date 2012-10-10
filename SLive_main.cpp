d981cd61658a4fd12cf2553526c630bb30bd921d
3Lu <eluminat001@gmx.de>
2012-10-10 15:43:17 +0200

    Add main-file

---
+++ SLive_main.cpp
@@ -0,0 +1,16 @@
+//
+//  SLive_main.cpp
+//  SLive
+//
+//  Created by Markus Scholl on 10.10.12.
+//  Copyright (c) 2012 CLMM. All rights reserved.
+//
+
+#include <iostream>
+#include "CServer_Manager.h"
+
+int main()
+{
+    
+    return 0;
+}
