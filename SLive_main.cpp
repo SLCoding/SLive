3d2eba7427c990c7e53055f5a441ebf248c7b359
Marcus Schütte <mail@m-schuette.eu>
2012-10-18 11:38:15 +0200

    Added queue reference

--- SLive_main.cpp
+++ SLive_main.cpp
@@ -11,6 +11,14 @@
 #include "CMusic_Server.h"
 #include "CClient.h"
 #include <iostream>
+
+#include <string>
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#include "CQueue.h"
+
 using namespace std;
 
 int main()
@@ -18,14 +26,31 @@ int main()
     CChat_Server chat;
     CMusic_Server music;
     CClient client;
+    CLogger logger;
     
-    int ret1 = chat.start(NULL, CChat_Server_run);
-    int ret2 = music.start(NULL, CMusik_Server_run);
-    int ret3 = client.start(NULL, CClient_run);
+    //int ret1 = chat.start(NULL, CChat_Server_run);
+    //int ret2 = music.start(NULL, CMusik_Server_run);
+    //int ret3 = client.start(NULL, CClient_run);
+    int ret4 = logger.start(NULL, CLogger_run);
     
-    cout << "ret: " << ret1 << endl;
-    chat.join(ret1, NULL);
-    music.join(ret2, NULL);
-    client.join(ret3, NULL);
+    //gcout << "ret: " << ret1 << endl;
+    
+    
+    
+
+    
+
+    
+    /* hole die Messagetypnummer aus dem ersten Parameter */
+    CQueue msg_queue(8300);
+    
+    msg_queue.send_msg("Main-Fehler!", 3);
+    
+    
+    
+   // chat.join(ret1, NULL);
+    //music.join(ret2, NULL);
+    //client.join(ret3, NULL);
+    logger.join(ret4, NULL);
     return 0;
 }
