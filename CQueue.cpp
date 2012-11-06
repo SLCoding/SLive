85180b68481ee127d3db735a880cefef67a87ad3
Marcus Schütte <mail@m-schuette.eu>
2012-11-06 10:20:17 +0100

    CQUEUE: minor changes

--- CQueue.cpp
+++ CQueue.cpp
@@ -16,28 +16,94 @@ CQueue::CQueue()
 
 CQueue::CQueue(int queue_id)
 {
+    default_type = 1;
+    send_type = 0;
+    rec_type = 0;
+    
     //create 2 queues
     //first for sending messages
     //second for sending following parts of a message
     //
     //this is required for maintaining order of the messages
-    queue_ref = msgget(queue_id, IPC_CREAT | 0666);
+    queue_ref = msgget(queue_id,  IPC_CREAT | 0666);
     queue_cont_ref = msgget(1111, IPC_CREAT | 0666);
-    default_type = 1;
+    
+    if(queue_ref == -1 || queue_cont_ref == -1)
+        throw "Couldn't create message queue: " + string(strerror(errno));
+
+}
+
+
+bool CQueue::set_type(long type)
+{
+    if(type !=0)
+    {
+        default_type = type;
+        return true;
+    }
+    else 
+    {
+        throw string("Couldn't set type: Zero is no valid message type");
+        return false;
+    }
+    return false;
+}  
+
+
+bool CQueue::set_send_type(long type)
+{
+    if(type >=0)
+    {
+        send_type = type;
+        return true;
+    }
+    else 
+    {
+        throw string("Couldn't set type: Negative types are not valid for sending");
+        return false;
+    }
+    return false;
+    
+}
+bool CQueue::set_rec_type(long type)
+{
+    send_type = type;
+    return true;
     
 }
 
 
-bool CQueue::set_type(int type)
-{
-    default_type = type; 
-    return true;
-}  
+
 
 
 bool CQueue::send_msg(string msg, long type) const
 {
-    int length = msg.length();
+    if(type <0)
+    {
+        throw string("Couldn't set type: Negative types are not valid for sending");
+        return false;
+    }
+    else if(type == 0)
+    {
+        if(send_type == 0)
+        {
+            if(default_type == 0)
+            {
+                throw string("Couldn't set type: Zero is no valid message type");
+                return false;
+            }
+            else
+            {
+                type = default_type;
+            }
+        }
+        else
+        {
+            type = send_type;
+        }
+    }
+    
+    long length = msg.length();
     int struct_size = sizeof(msg_part) - sizeof(long);
     
     //calculate random type number for following parts
@@ -81,6 +147,11 @@ bool CQueue::send_msg(string msg, long type) const
 
 t_msg CQueue::receive_msg(long type) const
 {
+    if(type == 0)
+        throw string("Couldn't set type: Zero is no valid message type");
+    
+    
+    
     string msg_str;
     
     msg_part message;
@@ -109,6 +180,10 @@ t_msg CQueue::receive_msg(long type) const
 
 t_msg CQueue::get_msg(long type) const
 {
+    if(type == 0)
+        throw string("Couldn't set type: Zero is no valid message type");
+    
+    
     string msg_str;
     
     msg_part message;
@@ -141,7 +216,13 @@ t_msg CQueue::get_msg(long type) const
 
 bool CQueue::destroy()
 {
-    return msgctl(queue_ref, IPC_RMID, NULL) + 1;
+    if(msgctl(queue_ref, IPC_RMID, NULL)==0)
+        return true;
+    else
+    {
+        throw string("Couldn't destroy message queue: ") + string(strerror(errno));
+        return false; 
+    }
     
     
 }
@@ -150,13 +231,13 @@ bool CQueue::destroy()
 
 const CQueue& CQueue::operator<<(const string& s) const
 {
-    this->send_msg(s, default_type);
+    this->send_msg(s, 0);
     return *this;
 }
 
 const CQueue& CQueue::operator>>(string &s)
 {
-    s = receive_msg(default_type).text;
+    s = receive_msg(0).text;
     return *this;
 }
 
@@ -165,13 +246,13 @@ const CQueue& CQueue::operator>>(string &s)
 const CQueue& CQueue::operator<<(const char* s) const
 {
     
-    this->send_msg(string(s), default_type);
+    this->send_msg(string(s), 0);
     return *this;
 }
 
 
 const CQueue& CQueue::operator>>(t_msg &s)
 {
-    s = this->receive_msg(default_type);
+    s = this->receive_msg(0);
     return *this;
 }
