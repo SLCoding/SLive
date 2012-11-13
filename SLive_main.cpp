ef2dc1a7559308312932907078e0d74eaa4c4842
3Lu <eluminat001@gmx.de>
2012-11-13 10:50:32 +0100

    Slive_main: minor changes

--- SLive_main.cpp
+++ SLive_main.cpp
@@ -26,69 +26,11 @@ using namespace std;
 
 int main()
 {
-    /*
-    CDatabase_Connection db;
-    map<string, string> result;
-    db.setUsername("root");
-    db.setPassword("");
-    db.setDB("stundenplan");
-    db.setHost("127.0.0.1");
-    if(db.connect())
-    {
-        cout << "db connection OK" << endl;
-        cout << "serverinfo: " << db.get_server_info() << endl;
-        cout << "serverversion: " << db.get_server_version()<< endl;
-        cout << "clientinfo: " << db.get_client_info() << endl;
-        cout << "clientversion: " << db.get_client_version() << endl;
-        cout << "host info: " << db.get_host_info() << endl;
-        cout << "proto info: " << db.get_proto_info() << endl;
-        cout << "thread id: " << db.thread_id() << endl;
-        cout << "character_set_name: " << db.character_set_name() << endl;
-        cout << "debug_info: " << db.dump_debug_info() << endl;
-        cout << "stat: " << db.stat() << endl;
-    }
-    else
-    {
-        cout << "fehler: " << db.error() << endl; 
-    }
-    string sql = "SELECT * FROM stundenplan;";
-    if(db.query(sql, (int)sql.length()))
-    {
-        cout << "query ok" << endl;
-    }
-    else
-    {
-        cout << "query not ok!" << endl;
-    }
-
-    cout << "affected rows: " << db.affected_rows() << endl;
-    try
-    {
-        int i = 0;
-        while(i < db.affected_rows())
-        {
-            result = db.fetch_assoc() ;
-            cout << "Raum: " << result["Raum"] << endl;
-            i++;
-        }
-    }
-    catch(char *e)
-    {
-        cout << e << endl;
-    }
-    catch(string e)
-    {
-        cout << e << endl;
-    }
-     */
-    CChat_Server chat;
-    CMusic_Server music;
     CLogger logger;
     
     logger.start(NULL, CLogger_run);
     CServer_Manager manager;
-    /* hole die Messagetypnummer aus dem ersten Parameter */
-    
+
     logger.join(NULL);
     
     return 0;
