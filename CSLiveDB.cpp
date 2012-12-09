73e8ee9a7fae80b78445114265271e4d3244557d
3Lu <eluminat001@gmx.de>
2012-12-09 10:38:35 +0100

    add japorties changes to my branch

--- CSLiveDB.cpp
+++ CSLiveDB.cpp
@@ -1,10 +1,10 @@
-    //
-    //  CSLiveDB.cpp
-    //  SLive
-    //
-    //  Created by Marcus Schütte on 14.11.12.
-    //  Copyright (c) 2012 CLMM. All rights reserved.
-    //
+//
+//  CSLiveDB.cpp
+//  SLive
+//
+//  Created by Marcus Schütte on 14.11.12.
+//  Copyright (c) 2012 CLMM. All rights reserved.
+//
 
 #include "CSLiveDB.h"
 
@@ -18,7 +18,7 @@ cUser::cUser(CSLiveDB db)
     this->db = db;
 }
 
-cUser::cUser(CSLiveDB db, int id, string name, string pwhash, string email)
+cUser::cUser(CSLiveDB db, long id, string name, string pwhash, string email)
 {
     this->db = db;
     this->id = id;
@@ -28,19 +28,19 @@ cUser::cUser(CSLiveDB db, int id, string name, string pwhash, string email)
 }
 
 
-cUser::cUser(CSLiveDB db, int id, string name, string pwhash, string email, list<cConference> conf_list, list<cUser> bdy_list)
+cUser::cUser(CSLiveDB db, long id, string name, string pwhash, string email, list<cConference> conf_list, list<cUser> bdy_list)
 {
     this->db = db;
     this->id = id;
     this->name = name;
     this->pwhash = pwhash;
     this->email = email;
-    this->conf_list = conf_list;
+    //this->conf_list = conf_list;
     this->bdy_list = bdy_list;
 }
 
 
-int cUser::get_id()
+long cUser::get_id()
 {
     return this->id;
 }
@@ -58,120 +58,161 @@ string cUser::get_server()
 }
 list<cConference> cUser::get_confList()
 {
-    return this->conf_list;
+    
+    stringstream query;
+    query << "SELECT * FROM conference WHERE user_id LIKE " << this->id << ";";
+    this->db.dbconn.query(query.str(), query.str().length());
+    
+    list<cConference> conf_list;
+    
+    for(int i = 0; i < this->db.dbconn.affected_rows(); i++)
+    {
+        map<string, string> result = this->db.dbconn.fetch_assoc();
+        conf_list.push_front(this->db.get_Conf(result["conf_id"]));
+    }
+    
+    return conf_list;
 }
 list<cUser> cUser::get_bdyList()
 {
     return this->bdy_list;
 }
 
-bool cUser::set_id(int id)
+
+
+user_status cUser::get_status()
 {
     stringstream query;
-
-    query<<"UPDATE user SET id = " << id << " WHERE id = " << this->id << ";";
+    query<<"SELECT status FROM user WHERE user_id = "<<this->id<<";";
+    
     this->db.dbconn.query(query.str(), query.str().length());
+    map<string, string> result = this->db.dbconn.fetch_assoc();
+    return (user_status)atoi(result["status"].c_str());
+    
+}
 
+
+
+
+
+
+bool cUser::set_id(long id)
+{
+    stringstream query;
+    
+    query<<"UPDATE user SET user_id = " << id << " WHERE user_id = " << this->id << ";";
+    this->db.dbconn.query(query.str(), query.str().length());
+    
     this->id = id;
     return true;
 }
 bool cUser::set_name(string name)
 {
     stringstream query;
-
-    query<<"UPDATE user SET name = '" << name << "' WHERE id = " << this->id << ";";
+    
+    query<<"UPDATE user SET name = '" << name << "' WHERE user_id = " << this->id << ";";
     this->db.dbconn.query(query.str(), query.str().length());
-
+    
     this->name = name;
-
+    
     return true;
 }
 bool cUser::set_email(string email)
 {
     stringstream query;
-
-    query<<"UPDATE user SET email = '" << email << "' WHERE id = " << this->id << ";";
+    
+    query<<"UPDATE user SET email = '" << email << "' WHERE user_id = " << this->id << ";";
     this->db.dbconn.query(query.str(), query.str().length());
-
+    
     this->email = email;
-
+    
     return true;
 }
 bool cUser::set_server(string server)
 {
     stringstream query;
-
-    query<<"UPDATE user SET server = '" << server << "' WHERE id = " << this->id << ";";
+    
+    query<<"UPDATE user SET server = '" << server << "' WHERE user_id = " << this->id << ";";
     this->db.dbconn.query(query.str(), query.str().length());
-
-
+    
+    
     this->server = server;
-
+    
     return true;
 }
+
+
+
+bool cUser::logout()
+{
+    this->set_server("");
+    this->set_status(OFFLINE);
+    return true;
+    
+}
+
 
 
 bool cUser::add_conf(string conf_id)
 {
     stringstream query;
-
-    query<<"INSERT INTO conference(id, user_id) VALUES('" << conf_id << "', " << this->id << ");";
+    
+    query<<"INSERT INTO conference(conf_id, user_id) VALUES('" << conf_id << "', " << this->id << ");";
     this->db.dbconn.query(query.str(), query.str().length());
-
-    this->conf_list.push_front(this->db.get_Conf(conf_id));
-
+    
     return true;
 }
 bool cUser::add_conf(cConference conf)
 {
     return this->add_conf(conf.get_id());
-
+    
 }
 
-bool cUser::add_bdy(int bdy_id)
+bool cUser::add_bdy(long bdy_id)
 {
     stringstream query;
-
+    
     query<<"INSERT INTO buddy(user_id, bdy_id) VALUES(" << this->id << ", " << bdy_id << ");";
     this->db.dbconn.query(query.str(), query.str().length());
-
+    
     this->bdy_list.push_front(this->db.get_User(bdy_id));
-
+    
     return true;
 }
 bool cUser::add_bdy(cUser bdy)
 {
     return add_bdy(bdy.get_id());
-
+    
 }
 
 
 bool cUser::del_conf(string conf_id)
-{
+{    
     stringstream query;
-
-    query<<"DELETE FROM conference WHERE id LIKE '"<< conf_id <<"'* AND user_id = "<< this->id <<";";
+    
+    query<<"DELETE FROM conference WHERE conf_id LIKE '"<< conf_id <<"'* AND user_id = "<< this->id <<";";
     this->db.dbconn.query(query.str(), query.str().length());
-
-    this->conf_list.remove(this->db.get_Conf(conf_id));
-
+    
     return true;
-
+    
+    
+    
+    
 }
 bool cUser::del_conf(cConference conf)
 {
     return this->del_conf(conf.get_id());
 }
 
-bool cUser::del_bdy(int bdy_id)
+bool cUser::del_bdy(long bdy_id)
 {
     stringstream query;
-
-    query<<"DELETE FROM buddy WHERE user_id = "<< this->id <<"'* AND bdy_id = "<< bdy_id <<";";
+    
+    query<<"DELETE FROM buddy WHERE user_id = "<< this->id <<"' AND bdy_id = "<< bdy_id <<";";
     this->db.dbconn.query(query.str(), query.str().length());
-
+    
     this->bdy_list.remove(this->db.get_User(bdy_id));
-
+    
     return true;
 }
 bool cUser::del_bdy(cUser bdy)
@@ -184,13 +225,13 @@ bool cUser::del_user()
     stringstream query;
     query<<"DELETE FROM conference WHERE user_id = "<< this->id <<";";
     this->db.dbconn.query(query.str(), query.str().length());
-
+    
     query<<"DELETE FROM buddy WHERE user_id = "<< this->id <<";";
+    this->db.dbconn.query(query.str(), query.str().length());    
+    
+    query<<"DELETE FROM user WHERE user_id = "<< this->id <<";";
     this->db.dbconn.query(query.str(), query.str().length());
-
-    query<<"DELETE FROM user WHERE id = "<< this->id <<";";
-    this->db.dbconn.query(query.str(), query.str().length());
-
+    
     return true;
 }
 
@@ -213,13 +254,12 @@ cConference::cConference(CSLiveDB db)
 {
     this->db = db;
 }
-cConference::cConference(CSLiveDB db, string id, list<cUser> usr_list)
+cConference::cConference(CSLiveDB db, string id)
 {
     this->db  = db;
     this->id = id;
-    this->usr_list = usr_list;
 }
-
+    
 
 string cConference::get_id()
 {
@@ -227,51 +267,64 @@ string cConference::get_id()
 }
 list<cUser> cConference::get_usrList()
 {
-    return this->usr_list;
+    
+    stringstream query;
+    
+    query<<"SELECT * FROM conference WHERE conf_id = '"<<this->id<<"'";
+    
+    list<cUser> usr_list;
+    
+    for(int i = 0; i < this->db.dbconn.affected_rows(); i++)
+    {
+        map<string, string> result = this->db.dbconn.fetch_assoc();
+        usr_list.push_front(this->db.get_User(atoi(result["user_id"].c_str())));
+    }
+    
+    return usr_list;
+    
+    
 }
-
+  
 bool cConference::set_id(string id)
 {
     stringstream query;
-
-    query<<"UPDATE conference SET id = '" << id << "' WHERE id LIKE '" << this->id << "';";
+    
+    query<<"UPDATE conference SET conf_id = '" << id << "' WHERE conf_id LIKE '" << this->id << "';";
     this->db.dbconn.query(query.str(), query.str().length());
-
+    
     this->id = id;
     return true;
 
 }
 /*bool cConference::set_usrList(list<cUser>)
- {
-
- }*/
-
-
-bool cConference::add_usr(int usr_id)
+{
+    
+}*/
+    
+    
+bool cConference::add_usr(long usr_id)
 {
     stringstream query;
-
-    query<<"INSERT INTO conference(id, user_id) VALUES('" << this->id << "', " << usr_id << ");";
+    
+    query<<"INSERT INTO conference(conf_id, user_id) VALUES('" << this->id << "', " << usr_id << ");";
     this->db.dbconn.query(query.str(), query.str().length());
-
-    this->usr_list.push_front(this->db.get_User(usr_id));
-
+    
+    
     return true;
 }
 bool cConference::add_usr(cUser usr)
 {
     return this->add_usr(usr.get_id());
 }
-
-bool cConference::del_usr(int usr_id)
+    
+bool cConference::del_usr(long usr_id)
 {
     stringstream query;
-
-    query<<"DELETE FROM conference WHERE id LIKE '"<< this->id <<"'* AND user_id = "<< usr_id <<";";
+    
+    query<<"DELETE FROM conference WHERE conf_id LIKE '"<< this->id <<"'* AND user_id = "<< usr_id <<";";
     this->db.dbconn.query(query.str(), query.str().length());
-
-    this->usr_list.remove(this->db.get_User(usr_id));
-
+    
+    
     return true;
 
 }
@@ -279,17 +332,17 @@ bool cConference::del_usr(cUser usr)
 {
     return this->del_usr(usr.get_id());
 }
-
+  
 bool cConference::del_conf()
 {
     stringstream query;
-
-    query<<"DELETE FROM conference WHERE id LIKE '"<< this->id <<"';";
+    
+    query<<"DELETE FROM conference WHERE conf_id LIKE '"<< this->id <<"';";
     this->db.dbconn.query(query.str(), query.str().length());
-
+    
     return true;
 }
-
+    
 
 
 
@@ -297,7 +350,7 @@ bool cConference::del_conf()
 
 CSLiveDB::CSLiveDB()
 {
-
+    
 }
 
 CSLiveDB::CSLiveDB(string user, string password, string DB, string Host, int port)
@@ -315,27 +368,38 @@ cConference CSLiveDB::create_conf(string id)
 {
     cConference conf = cConference(*this);
     conf.set_id(id);
-
+    
     return conf;
 }
 cConference CSLiveDB::create_conf(string id, list<cUser> usr_list)
 {
     cConference conf = cConference(*this);
     conf.set_id(id);
-
+    
     for(list<cUser>::iterator i = usr_list.begin(); i != usr_list.end(); i++)
     {
-        conf.usr_list.push_front((*i));
+        stringstream query;
+        
+        query<<"INSERT INTO conference(conf_id, user_id) VALUES('"<<id<<"', "<<i->get_id()<<");";
+        this->dbconn.query(query.str(), query.str().length());
+        
+        
     }
-
-        //db
-
+    
+    
     return conf;
 }
 
 cConference CSLiveDB::get_Conf(string id)
 {
-
+    stringstream query;
+    
+    query<<"SELECT * FROM conference WHERE conf_id LIKE '" << id <<"';";
+    this->dbconn.query(query.str(), query.str().length());
+    
+    cConference conf = cConference(*this, id);
+    
+    return conf;
 }
 
 
@@ -343,12 +407,12 @@ cConference CSLiveDB::get_Conf(string id)
 bool CSLiveDB::checkUsername(string name)
 {
     stringstream query;
-
+    
     query<<"SELECT * FROM user WHERE name LIKE '" << name <<"';";
     this->dbconn.query(query.str(), query.str().length());
     if(this->dbconn.affected_rows() > 0)
         return false;
-    else
+    else 
         return true;
 
 }
@@ -358,12 +422,12 @@ cUser CSLiveDB::create_User(string name, string pw)
     if(this->checkUsername(name))
     {
         stringstream query;
-        query<<"INSERT INTO user(name, pwhash, email) VALUES ('"<< name << "', '"<< pw << "', '');";
+        query<<"INSERT INTO user(name, pwhash, email) VALUES ('"<< name << "', '"<< md5(pw) << "', '');";
         this->dbconn.query(query.str(), query.str().length());
-
-
+        
+        
         return this->get_User(name);
-
+        
     }
     else
         throw "Username already in use";
@@ -375,24 +439,51 @@ cUser CSLiveDB::create_User(string name, string pw, string email)
     return usr;
 }
 
-cUser CSLiveDB::get_User(int id)
+cUser CSLiveDB::get_User(long id)
 {
     stringstream query;
-    query << "SELECT * FROM user WHERE id = " << id << ";";
+    query << "SELECT * FROM user WHERE user_id = " << id << ";";
     this->dbconn.query(query.str(), query.str().length());
-
+    
     map<string, string> result = this->dbconn.fetch_assoc();
-
-    cUser usr = cUser(*this, atoi(result["id"].c_str()), result["name"] , result["pwhash"], result["email"]);
-
-    query.str("");
-    query<<"SELECT * FROM conference WHERE usr_id = " << id << ";";
-    this->dbconn.query(query.str(), query.str().length());
-
-
-
+    
+    cUser usr = cUser(*this, atoi(result["user_id"].c_str()), result["name"] , result["pwhash"], result["email"]);
+    
+    return usr;
+    
 }
 cUser CSLiveDB::get_User(string name)
 {
+    stringstream query;
+    query << "SELECT * FROM user WHERE name LIKE '" << name << "';";
+    this->dbconn.query(query.str(), query.str().length());
     
+    map<string, string> result = this->dbconn.fetch_assoc();
+    
+    cUser usr = cUser(*this, atoi(result["user_id"].c_str()), result["name"] , result["pwhash"], result["email"]);
+    
+    return usr;
 }
+
+
+cUser CSLiveDB::login(string name, string pw, string server)
+{
+    return this->login(this->get_User(name).get_id(), pw, server);
+}
+
+cUser CSLiveDB::login(long id, string pw, string server)
+{
+    cUser usr = this->get_User(id);
+    if(usr.pwhash == md5(pw))
+    {
+        usr.set_server(server);
+        usr.set_status(ONLINE);
+    }
+    else {
+        throw 1;
+    }
+    
+    return usr;
+}
+
+
--- CSLiveDB.h
+++ CSLiveDB.h
@@ -1,21 +1,14 @@
-    //
-    //  CSLiveDB.h
-    //  SLive
-    //
-    //  Created by Marcus Schütte on 14.11.12.
-    //  Copyright (c) 2012 CLMM. All rights reserved.
-    //
+//
+//  CSLiveDB.h
+//  SLive
+//
+//  Created by Marcus Schütte on 14.11.12.
+//  Copyright (c) 2012 CLMM. All rights reserved.
+//
 
 
-    //login
-    //logging
-    //offline nachrichten
-    //
-
-
-
-
-
+//logging
+//---offline nachrichten
 
 
 
@@ -30,10 +23,8 @@
 #include <sstream>
 #include <stdlib.h>
 #include "CDatabase_Connection.h"
+#include "md5.h"
 using namespace std;
-
-
-
 
 
 class cConference;
@@ -46,91 +37,97 @@ class CSLiveDB
 {
     friend class cUser;
     friend class cConference;
-
+    
 private:
     CDatabase_Connection dbconn;
-
-
+    
+    
 protected:
-
-
+    
+    
 public:
     CSLiveDB();
     CSLiveDB(string user, string password, string DB, string Host = "127.0.0.1", int port = 3306);
-
-
-
+    
+    
+    
     cConference create_conf();
     cConference create_conf(string id);
     cConference create_conf(string id, list<cUser> usr_list);
-
+    
     cConference get_Conf(string id);
-
-
-
+    
+    
+    
     bool checkUsername(string name);
-
+    
     cUser create_User(string name, string pw);
     cUser create_User(string name, string pw, string email);
-
-    cUser get_User(int id);
+    
+    cUser get_User(long id);
     cUser get_User(string name);
+    cUser login(string name, string pw, string server);
+    cUser login(long id, string pw, string server);
 };
 
 
 
+enum user_status {OFFLINE, BUSY, AWAY, ONLINE};
 
 class cUser
 {
     friend class CSLiveDB;
 private:
     cUser(CSLiveDB db);
-    cUser(CSLiveDB db, int id, string name, string pwhash, string email);
-    cUser(CSLiveDB db, int id, string name, string pwhash, string email, list<cConference> conf_list, list<cUser> bdy_list);
-
-
+    cUser(CSLiveDB db, long id, string name, string pwhash, string email);
+    cUser(CSLiveDB db, long id, string name, string pwhash, string email, list<cConference> conf_list, list<cUser> bdy_list);
+    
+    
     CSLiveDB db;
-
-    int id;
+    
+    long id;
     string name;
     string pwhash;
     string email;
     string server;
-    list<cConference> conf_list;
-    list<cUser> bdy_list;
-
+    //list<cConference> conf_list;
+    list<cUser> bdy_list;    
+    
 public:
-
-
-
-    int get_id();
+    
+    
+    
+    long get_id();
     string get_name();
     string get_email();
     string get_server();
+    user_status get_status();
     list<cConference> get_confList();
     list<cUser> get_bdyList();
-
-    bool set_id(int id);
+    
+    bool set_id(long id);
     bool set_name(string name);
     bool set_email(string email);
     bool set_server(string server);
-
-
+    bool set_status(user_status status);
+    
+    bool logout();
+    
     bool add_conf(string conf_id);
     bool add_conf(cConference conf);
-
-    bool add_bdy(int bdy_id);
+    
+    bool add_bdy(long bdy_id);
     bool add_bdy(cUser bdy);
-
-
+    
+    
     bool del_conf(string conf_id);
     bool del_conf(cConference conf);
-
-    bool del_bdy(int bdy_id);
+    
+    bool del_bdy(long bdy_id);
     bool del_bdy(cUser bdy);
-
+    
     bool del_user();
-
+    
 };
 
 
@@ -140,28 +137,29 @@ class cConference
     friend class CSLiveDB;
 private:
     cConference(CSLiveDB db);
+    cConference(CSLiveDB db, string id);
     cConference(CSLiveDB db, string id, list<cUser> usr_list);
     cConference(CSLiveDB db, list<cUser> usr_list);
-
+    
     CSLiveDB db;
-
+    
     string id;
-    list<cUser> usr_list;
-
-
-
+    //list<cUser> usr_list;
+    
+    
+    
 public:
     string get_id();
     list<cUser> get_usrList();
-
+    
     bool set_id(string id);
-        //bool set_usrList(list<cUser>);
-
-
-    bool add_usr(int usr_id);
+    //bool set_usrList(list<cUser>);
+    
+    
+    bool add_usr(long usr_id);
     bool add_usr(cUser usr);
-
-    bool del_usr(int usr_id);
+    
+    bool del_usr(long usr_id);
     bool del_usr(cUser usr);
     
     bool del_conf();
